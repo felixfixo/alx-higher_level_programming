3b7467f82ec79c6ed3db6ed4b6a34dc49210c9c3
felixfixo <fixoothina@gmail.com>
2022-06-29 06:57:01 +0300

    changes

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for char in range(97, 123, -1):
-	if char % 2 == 0:
-		print('{:c}'.format(char - 32), end='')
-	print('{:c}'.format(char), end='')
+for i in range(ord('z'), ord('a') - 1, -1):
+    if (i % 2 == 0):
+        print("{}".format(chr(i)), end="")
+    else:
+        print("{}".format(chr(i - 32)), end="")
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-	list1 = list(str)
-	i = n
-	for i in range(n, len[list1]):
-		list1[i] = list1[i + 1]
-	str = ''.join(list1)
-	return str
+    if len(str) >= n and n >= 0:
+        str = str[0:n] + str[n + 1:]
+        return str
+    else:
+        return 
