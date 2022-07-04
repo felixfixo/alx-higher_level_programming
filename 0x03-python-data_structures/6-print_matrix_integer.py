b2bf88482888767fc2e482301af275b866976791
felixfixo <fixoothina@gmail.com>
2022-07-04 16:58:01 +0300

    first commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" prints a matrix of integers
+"""
+
+def print_matrix_integer(matrix=[[]]):
+    if matrix != [[]]:
+        for row in matrix:
+            for elem in row:
+                print("{:d}".format(elem), end=" "
+                      if elem != row[-1] else '\n')
+    else:
+        print()
